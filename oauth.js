9b6f0928a1eb98be71625790538095ff7b29c582
JungHyeonWoo <wjdgusdn1223@gmail.com>
2018-12-04 13:56:03 +0900

    reminder, auth refresh function complete shortcut function ongoing

--- oauth.js
+++ oauth.js
@@ -6,69 +6,9 @@ window.onload = function() {
     //  일정 추가시 캘린더에 일정 추가 후 팝업 창 종료
     document.querySelector('#complete_btn').addEventListener('click', function() {
         //  주의!! manifest.json 파일의 scopes가 비어있는 값이면 오류발생함
-        chrome.identity.getAuthToken({interactive: true}, function(token) {
-            console.log(token);
-            
-            //  시작 날짜 종료 날짜 분류
-            var st = $('#start').calendar('get date') + "";
-            var et = $('#end').calendar('get date') + "";
+        chrome.identity.getAuthToken({interactive: true}, function(token){addSchedule(token);}
+    )});
 
-            var stArray = st.split(' ');
-            var etArray = et.split(' ');
-            
-            stArray[1] = monthConvert(stArray[1]);
-            etArray[1] = monthConvert(etArray[1]);
-
-            //  post로 요청
-            fetch("https://www.googleapis.com/calendar/v3/calendars/primary/events?"+
-                    "sendUpdates=all&key=AIzaSyDsyAyx4zEUsErwp__HN5yUnmmcekuMj28", 
-                {  
-                    method: 'POST',  
-                    async: true,
-                    headers: {
-                        Authorization: 'Bearer ' + token,
-                        'Content-Type': 'application/json'
-                    },
-                    'contentType': 'json',
-                    body: JSON.stringify({
-                        "start":
-                        {
-                            "dateTime": stArray[3] + "-" + stArray[1] + "-" + stArray[2] + "T" + stArray[4] + "+09:00"
-                        },
-                        "end":
-                        {
-                            "dateTime": etArray[3] + "-" + etArray[1] + "-" + etArray[2] + "T" + etArray[4] + "+09:00"
-                        },
-                        "colorId":"1",
-                        "visibility":"default",
-                        "reminders":
-                        {
-                            "useDefault": false,
-                            "overrides":
-                            [
-                                {
-                                    "method": "email",
-                                    "minutes": document.getElementById('minute').value
-                                },
-                                {
-                                    "method": "popup",
-                                    "minutes": document.getElementById('minute').value
-                                }
-                            ]
-                        },
-                        "description": document.getElementById('desc').value,
-                        "summary": document.getElementById('title').value
-                    })
-                }
-            )
-            .then(function (data) {  
-                console.log('Request success: ', data);  
-            })  
-            .catch(function (error) {  
-                console.log('Request failure: ', error);  
-            });
-        });
-    });
     // 시작-종료 날짜 입력
     $('#start').calendar({
         type: 'datetime',
@@ -80,7 +20,116 @@ window.onload = function() {
         ampm: false,
         monthFirst: false
     });
-};
+}
+
+function addSchedule(token){
+    console.log(token);
+    
+    //  시작 날짜 종료 날짜 분류
+    var st = $('#start').calendar('get date') + "";
+    var et = $('#end').calendar('get date') + "";
+
+    var stArray = st.split(' ');
+    var etArray = et.split(' ');
+    
+    stArray[1] = monthConvert(stArray[1]);
+    etArray[1] = monthConvert(etArray[1]);
+    
+    //  알람 요청 조건에 따라 설정
+    var rArray = document.getElementsByName('reminder');
+
+    var selReminder = rArray[0];
+
+    for(var i = 0; i < rArray.length; i++){
+        if(rArray[i].checked){
+            selReminder = rArray[i];
+        }
+    }
+
+    switch(selReminder.id){
+        case "none":
+            selReminder = {
+                "useDefault": true
+            }
+            break;
+        case "popup":
+            selReminder = {
+                "useDefault": false,
+                "overrides": [
+                    {
+                        "method": "popup",
+                        "minutes": document.getElementById('minute').value ? document.getElementById('minute').value : '0'
+                    }
+                ]
+            }
+            break;
+        case "email":
+            selReminder = {
+                "useDefault": false,
+                "overrides": [
+                    {
+                        "method": "email",
+                        "minutes": document.getElementById('minute').value ? document.getElementById('minute').value : '0'
+                    }
+                ]
+            }
+            break;
+        case "both":
+            selReminder = {
+                "useDefault": false,
+                "overrides": [
+                    {
+                        "method": "popup",
+                        "minutes": document.getElementById('minute').value ? document.getElementById('minute').value : '0'
+                    },
+                    {
+                        "method": "email",
+                        "minutes": document.getElementById('minute').value ? document.getElementById('minute').value : '0'
+                    }
+                ]
+            }
+            break;
+    }
+
+    //  post로 요청
+    fetch("https://www.googleapis.com/calendar/v3/calendars/primary/events?"+
+            "sendUpdates=all&key=AIzaSyDsyAyx4zEUsErwp__HN5yUnmmcekuMj28", 
+        {  
+            method: 'POST',  
+            async: true,
+            headers: {
+                Authorization: 'Bearer ' + token,
+                'Content-Type': 'application/json'
+            },
+            'contentType': 'json',
+            body: JSON.stringify({
+                "start":
+                {
+                    "dateTime": stArray[3] + "-" + stArray[1] + "-" + stArray[2] + "T" + stArray[4] + "+09:00"
+                },
+                "end":
+                {
+                    "dateTime": etArray[3] + "-" + etArray[1] + "-" + etArray[2] + "T" + etArray[4] + "+09:00"
+                },
+                "colorId":"1",
+                "visibility":"default",
+                "reminders": selReminder,
+                "description": document.getElementById('desc').value,
+                "summary": document.getElementById('title').value
+            })
+        }
+    )
+    .then(function (data) {  
+        console.log('Request success: ', data.status);
+        if(data.status != 200)
+            chrome.identity.removeCachedAuthToken({token: token},function(){
+                chrome.identity.getAuthToken({interactive: true},function(token){addSchedule(token);});
+            });
+    })  
+    .catch(function (error) {  
+        console.log('Request failure: ', error);  
+    });
+}
 
 function monthConvert(month){
     switch(month){
